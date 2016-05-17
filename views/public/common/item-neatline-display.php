8648a42b952ee05127201ac5ee02e06a3a5b00fd
Jeremy Guillette <jaguillette@gmail.com>
2016-05-17 10:45:40 -0400

    now only displays when metadata is present

--- views/public/common/item-neatline-display.php
+++ views/public/common/item-neatline-display.php
@@ -1,10 +1,12 @@
 <?php
-if ($neatlineExhibit = metadata('item', array('Neatline Display','Neatline Exhibit'))) {
+echo current_url();
+if ($neatlineExhibit = metadata('item', array('Neatline Display','Neatline Exhibit'))):
 	if ($neatlineRecord = metadata('item', array('Neatline Display','Neatline Record'))) {
 		# neatlineRecord is set
 	} else { $neatlineRecord = "";}
-}
 ?>
 <div id="item-neatline-display">
 	<iframe src="<?php echo url('neatline/fullscreen/'.$neatlineExhibit.'#'.$neatlineRecord); ?>" width="100%" height="500"></iframe>
+	<a href="<?php echo url('neatline/fullscreen/'.$neatlineExhibit.'#'.$neatlineRecord); ?>">View Fullscreen</a>
 </div>
+<?php endif ?>
